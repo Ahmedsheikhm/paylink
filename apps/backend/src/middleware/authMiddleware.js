76148e81c66128836bae84a05f782a0d2d7ae7bd
Sheikh Muhammad Ahmed <ahmedsheikhmuhammad01@gmail.com>
2025-10-13 17:33:34 -0700

    feat(auth): add JWT auth middleware for protected routes

---
+++ apps/backend/src/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import prisma from "../config/prisma";
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const requireAuth = async (req,res,next)=>{
+    try{
+        const auth = req.headers.autherization;
+        if(!auth || !auth.startswith('Bearer')){
+            return res.status(401).json({message : 'Autherization required'});
+        }
+
+        const token  =auth.split(' ')[1];
+        const payload = jwt.verify(token,JWT_SECRET);
+
+        //await user fetch from DB
+        const user = await prisma.user.findUnique({where : {id:payload.userId}});
+        if(!user){
+            return res.status(401).json({message : 'Invalid token (user not found)'});
+        }
+        req.user = {id:user.id,name:user.name,email:user.email};
+        return next();
+    }catch(err){
+        console.error('auth middleware error',err);
+        return res.status(401).json({message : 'Invalid or expired token'});
+    }
+};
